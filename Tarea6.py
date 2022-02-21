eaacd1583e257fcf9ae4ecfce6e7eebb7d3880d6
Lineth13 <100149446+Lineth13@users.noreply.github.com>
2022-02-21 10:56:33 -0500

    Add files via upload

---
+++ Tarea6.py
@@ -0,0 +1,8 @@
+nombre = input("ingresa tu nombre: ")
+numero = int(input("ingresa el numero: "))
+
+for i in range(numero):
+    print(nombre)
+    
+    
+    
---
+++ Tarea7.py
@@ -0,0 +1,12 @@
+print ("-" *30)
+print ("Tablas De Multiplicar")
+print ("-" *30)
+
+
+numero = int(input("Ingrese un numero: "))
+for i in range (11):
+    print(numero, "X", i, "=", numero * i)
+    
+    
+    
+    
